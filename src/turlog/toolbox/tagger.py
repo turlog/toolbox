5e37820687e010a72cdc8078c2d910e2cdc8f724
Łukasz Zosiak <lukasz.zosiak@gmail.com>
2025-04-22 13:02:47 +0200

    MP3 tagger -  detecting mismatches

--- src/turlog/toolbox/tagger.py
+++ src/turlog/toolbox/tagger.py
@@ -2,9 +2,45 @@ import re
 import click
 
 from pathlib import Path
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 from platformdirs import user_cache_path
 from mutagen.easyid3 import EasyID3 as ID3
+
+
+TAGS = {
+    "title": True,
+    "artist": True,
+}
+
+
+def get_tags_from_name(path: Path, pattern: re.Pattern):
+    """
+    Extract tags from filename using a regex pattern.
+    """
+    if (match := pattern.match(path.name)) is not None:
+        return match.groupdict()
+    return {}
+
+
+def get_tags_from_idv3(path: Path):
+    """
+    Extract tags from ID3v3 metadata.
+    """
+    tags = ID3(path)
+    return {tag: tags.get(tag)[0] for tag in tags.keys()}
+
+
+def scan_file(path: Path, pattern: re.Pattern):
+    """
+    Scan a single file for tags.
+    """
+    tags_from_name = get_tags_from_name(path, pattern)
+    if tags_from_idv3 := get_tags_from_idv3(path):
+        for key, detected in tags_from_idv3.items():
+            if not (existing := tags_from_name.get(key, detected)) == detected:
+                print(f"Tag mismatch for {key}: {existing!r} != {detected!r} @ {path}")
+    return tags_from_idv3 | tags_from_name
 
 
 @click.group()
@@ -31,22 +67,26 @@ def tagger_scan(source: Path, pattern: re.Pattern, cache: Path):
     """
     Scan selected SOURCE files / folders for MP3 files and add to the cache.
     """
-    TAGS = {
-        "title": True,
-        "artist": True,
-    }
-    for path in source.rglob("**/*.mp3"):
-        fileinfo = {
-            "path": path,
-        }
-        if (match := pattern.match(path.name)) is not None:
-            from_name = fileinfo["tags_from_name"] = match.groupdict()
-        if tags := ID3(path):
-            from_idv3 = fileinfo["tags_from_idv3"] = {
-                tag: tags.get(tag)[0] for tag, required in TAGS.items() if required
-            }
-        if not from_name == from_idv3:
-            print(fileinfo)
+    if missing := {tag for tag, required in TAGS.items() if required} - set(
+        pattern.groupindex.keys()
+    ):
+        raise click.BadParameter(
+            f"Pattern lacks following named groups: {', '.join(missing)}.\n"
+            "Use '(?P<spam>.*)' syntax to add named group with name 'spam', make sure your shell is not interpreting it."
+        )
+    with ThreadPoolExecutor() as executor:
+        for future in as_completed(
+            [
+                executor.submit(scan_file, path, pattern)
+                for source in source
+                for path in source.rglob("**/*.mp3")
+                if path.is_file()
+            ]
+        ):
+            try:
+                future.result()
+            except Exception as e:
+                click.echo(f"Error processing file: {e}", err=True)
 
 
 if __name__ == "__main__":
